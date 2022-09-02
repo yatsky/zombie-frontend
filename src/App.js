3232d131f96fdd1698a3fa04e25f666041d353e0
Yaoni <y.wang7@uq.net.au>
2022-09-02 22:50:00 +1000

    Add grid and post logic.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square(props) {
+    return (
+        <button className="square">
+            {props.value}
+        </button>
+    );
 }
 
-export default App;
+function Board(props) {
+    const [state, setState] = useState(
+        {
+            squares: Array(9).fill(null).map((_, i) => i),
+            gridSize: 4,
+            gameConfig:
+                {
+                    "gridSize": 4, "commands": "UDRR", "zombie": {
+                        "x": 1,
+                        "y": 2
+                    },
+
+                    "creatures": [
+                        {"x": 1, "y": 0},
+                        {"x": 1, "y": 1}
+                    ]
+                },
+            boardSetup: {
+                "creatures": [],
+                "zombies": [],
+            }
+        }
+    )
+
+    const handlePlay = e => {
+        axios.post(`http://localhost:8080/start`, state.gameConfig)
+            .then(res => {
+                    setState(prevState => {
+                        return {
+                            ...prevState, boardSetup: {
+                                "creatures": res.data.creatures,
+                                "zombies": res.data.zombies,
+                            }
+                        }
+                    })
+                }
+            )
+    }
+
+    const renderSquare = (name) => {
+        return (
+            <Square
+                value={name}
+            />
+        );
+    }
+
+    const renderSquares = (gridSize) => {
+        return Array(gridSize).fill(null).map((_, y) => {
+            return <div className="board-row">
+                {
+                    Array(gridSize).fill(null).map((_, x) => {
+                            return renderSquare("N")
+                    })
+                }
+            </div>
+        })
+    }
+
+    const status = 'Zombie Game Console';
+
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <button onClick={handlePlay}>Play</button>
+            {renderSquares(state.gridSize)}
+        </div>
+
+    );
+}
+
+class Game extends React.Component {
+
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Game
